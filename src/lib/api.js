4fc78fdf4d40a2e99776a071cc87278794853422
Issax Pi <e6705916@gmail.com>
2023-10-13 18:54:17 +0600

    Create api.js

---
+++ src/lib/api.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const API_URL = process.env.WP_URL;
+
+async function fetchAPI(query, { variables } = {}) {
+    const headers = { 'Content-Type': 'application/json' };
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ query, variables }),
+    });
+
+    const json = await res.json();
+    if (json.errors) {
+        console.log(json.errors);
+        throw new Error('Failed to fetch API');
+    }
+
+    return json.data;
+}
+
+export async function getAllPagesWithSlugs() {
+    const data = await fetchAPI(`
+    {
+      pages(first: 10000) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+    `);
+    return data?.pages;
+}
